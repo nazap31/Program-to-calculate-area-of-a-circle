7fa799c3dc56a43ac13d1f211d1612f3abdb9aad
nazap31 <you@example.com>
2022-06-14 03:03:38 -0600

    Program to calculate area of a circle

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Calcular el area de un circulo
+
+//radio
+int radio;
+Console.WriteLine("Porfavor ingrese el radio");
+radio = int.Parse(Console.ReadLine());
+
+//pi
+double pi = 3.14D;
+
+//area
+double area;
+area = (radio * radio * pi);
+Console.Write("El area es " );
+Console.Write (area);
